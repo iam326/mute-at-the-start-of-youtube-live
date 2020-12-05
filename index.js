decc61a13cc5e595205f322263b24e8acebad48f
takahashi.tatsunari <takahashi.tatsunari@classmethod.jp>
2020-12-06 00:30:07 +0900

    強制的にmute化する

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-document.querySelector('button#button[aria-label^="YouTube アプリ"]').click();
+(function () {
+  const intervalId = setInterval(mute, 1000);
+
+  function mute() {
+    console.log('=== start mute() ===');
+    const button = document.querySelector(
+      'button#button[aria-label="ミュート"]'
+    );
+    if (button) {
+      button.click();
+      clearInterval(intervalId);
+    }
+  }
+})();
